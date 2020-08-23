489c20e079add9880a742d18aa5b128cb376d0d1
Max <maxwil2001@hotmail.com>
2020-08-23 13:49:03 +0200

    circle fil

---
+++ Topdown horror game/Assets/Scripts/UI/FillCircle.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillCircle : MonoBehaviour
+{
+    [Header("image of the circle")]
+    public Image fillImage;
+
+    [Header("speed the circle gets filled")]
+    public float fillSpeed = 1.0f;
+
+    private float value = 0.0f;
+
+    void Update()
+    {
+        FillCircles();
+    }
+
+    //fills the circle in when the player holds down the E button
+    private void FillCircles()
+    {
+        if(value < 100.0f)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                value += fillSpeed * Time.deltaTime;
+            }
+            else
+            {
+                value -= fillSpeed * Time.deltaTime;
+
+                if(value <= 0)
+                {
+                    value = 0.0f;
+                }
+            }
+
+            fillImage.fillAmount = value / 100;
+        }
+    }
+}
