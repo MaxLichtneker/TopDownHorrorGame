8b4fa0e187925187fc4af7f82a001f5c10dad9c5
Max <maxwil2001@hotmail.com>
2020-08-25 13:19:36 +0200

    door screws spin and go in the door one by one

--- Topdown horror game/Assets/Scripts/UI/FillCircle.cs
+++ Topdown horror game/Assets/Scripts/UI/FillCircle.cs
@@ -11,11 +11,31 @@ public class FillCircle : MonoBehaviour
     [Header("speed the circle gets filled")]
     public float fillSpeed = 1.0f;
 
+    [Header("all of the screws to the door")]
+    [SerializeField] private GameObject[] screws;
+
+    [Header("replacement screw for when the screw is done spinning")]
+    [SerializeField] private GameObject replacementScrew;
+
+    private Stack<GameObject> screwStack = new Stack<GameObject>();
+
     private float value = 0.0f;
+
+    private int amountOfTurns = 4;
+
+    private void Start()
+    {
+        screwStack.Push(screws[0]);
+        screwStack.Push(screws[1]);
+        screwStack.Push(screws[2]);
+        screwStack.Push(screws[3]);
+    }
 
     void Update()
     {
         FillCircles();
+
+        ActivateScrew();
     }
 
     //fills the circle in when the player holds down the E button
@@ -23,9 +43,10 @@ public class FillCircle : MonoBehaviour
     {
         if(value < 100.0f)
         {
-            if (Input.GetKey(KeyCode.E))
+            if (Input.GetKey(KeyCode.E) && amountOfTurns > 0)
             {
                 value += fillSpeed * Time.deltaTime;
+                TurnScrew();
             }
             else
             {
@@ -40,4 +61,36 @@ public class FillCircle : MonoBehaviour
             fillImage.fillAmount = value / 100;
         }
     }
+
+    private void ActivateScrew()
+    {
+        if(fillImage.fillAmount >= 1.0f)
+        {
+            if(screwStack != null)
+            {
+                for (int i = 0; i < screws.Length; i++)
+                {
+                    Instantiate(replacementScrew, transform.position,transform.rotation);
+                    screws[i].SetActive(false);
+                }
+
+                screwStack.Pop().SetActive(true);
+                amountOfTurns--;
+                fillImage.fillAmount = 0.0f;
+                value = 0.0f;
+            }
+        }
+    }
+
+    private void TurnScrew()
+    {
+        for (int i = 0; i < screws.Length; i++)
+        {
+            if(screws[i] != null)
+            {
+                screws[i].transform.Rotate(0, 0, -120 * Time.deltaTime);
+            }
+        }
+    }
+
 }
